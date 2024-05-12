b3be7c3fe7ca4a5a2cbc6cd81123e73a8eee9fdf
AlvaroQP <alonsol1569@gmail.com>
2024-05-12 19:22:30 +0200

    Prueba mostrar json por consola

--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -1,10 +1,19 @@
 import { Login } from "@/ui/components/login/Login";
 
 const getAutoescuelas = async () => {
-    // const fetchData = await ()
+    const fetchData = await fetch("http://localhost:3000/api/login", {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+    const res = await fetchData.json();
+    console.log(res);
+    // return res;
 }
 
-export default function LoginPage() {
+export default async function LoginPage() {
+    getAutoescuelas();
     return (
         <div className="flex justify-center mt-52">
             <Login />
