fe430d75cca6922c9a20d23b1b13841e23d88956
AlvaroQP <alonsol1569@gmail.com>
2024-05-25 00:01:21 +0200

    Error de users arreglado

--- src/ui/components/users/ActionBar.tsx
+++ src/ui/components/users/ActionBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC } from "react";
 import { Button } from "../common";
 import { SearchInput } from "./SearchInput";
--- src/ui/components/users/UserRegistrationForm.tsx
+++ src/ui/components/users/UserRegistrationForm.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC, useState } from "react";
 import { Button, Title } from "../common";
 import { CloseIcon } from "../icons";
